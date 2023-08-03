01b010785200e6d49a9f24e7efce2d5a5f0bede0
SabaMuladze <muladze66@bk.ru>
2023-08-04 01:45:25 +0400

    refactor:add objects and orgizine code

--- script.js
+++ script.js
@@ -5,146 +5,73 @@ const toggle = document.querySelector('#checkbox');
 const priceSpan = document.querySelector('.month');
 
 
-function change() {
-
-    range.addEventListener('input', () => {
-
-        if (toggle.checked == true) {
-            if (range.value == 1) {
-                pageviews.textContent = '10K PAGEVIEWS';
-                price.textContent = '$72.00';
-                priceSpan.textContent = '/ Year';
-                range.style.background = '#ECF0FB'
-
-            }
-            else if (range.value == 2) {
-                pageviews.textContent = '50K PAGEVIEWS';
-                price.textContent = '$108.00';
-                priceSpan.textContent = '/ Year';
-                range.style.background = 'linear-gradient(to left,#ECF0FB  75%,#10D8C4  25%)'
-
-            }
-            else if (range.value == 3) {
-                pageviews.textContent = '100K PAGEVIEWS';
-                price.textContent = '$144.00';
-                priceSpan.textContent = '/ Year';
-                range.style.background = 'linear-gradient(to right, #10D8C4 50%, #ECF0FB 50%)'
-
-            }
-            else if (range.value == 4) {
-                pageviews.textContent = '500K PAGEVIEWS';
-                price.textContent = '$216.00';
-                priceSpan.textContent = '/ Year';
-                range.style.background = 'linear-gradient(to right, #10D8C4 75%, #ECF0FB 25%)'
-
-            }
-            else if (range.value == 5) {
-                pageviews.textContent = '1M PAGEVIEWS';
-                price.textContent = '$324.00';
-                priceSpan.textContent = '/ Year';
-                range.style.background = 'linear-gradient(to right, #10D8C4 100%, #ECF0FB 0%)'
-
-            }
-        }
-        else {
-            if (range.value == 1) {
-                pageviews.textContent = '10K PAGEVIEWS';
-                price.textContent = `$${8.00.toFixed(2)}`;
-                range.style.background = '#ECF0FB'
-            }
-            else if (range.value == 2) {
-                pageviews.textContent = '50K PAGEVIEWS';
-                price.textContent = `$${12.00.toFixed(2)}`;
-                range.style.background = 'linear-gradient(to left,#ECF0FB  75%,#10D8C4  25%)'
-
-            }
-            else if (range.value == 3) {
-                pageviews.textContent = '100K PAGEVIEWS';
-                price.textContent = `$${16.00.toFixed(2)}`;
-                range.style.background = 'linear-gradient(to right, #10D8C4 50%, #ECF0FB 50%)'
-
-            }
-            else if (range.value == 4) {
-                pageviews.textContent = '500K PAGEVIEWS';
-                price.textContent = `$${24.00.toFixed(2)}`;
-                range.style.background = 'linear-gradient(to right, #10D8C4 75%, #ECF0FB 25%)'
-
-            }
-            else if (range.value == 5) {
-                pageviews.textContent = '1M PAGEVIEWS';
-                price.textContent = `$${32.00.toFixed(2)}`;
-                range.style.background = 'linear-gradient(to right, #10D8C4 100%, #ECF0FB 0%)'
-
-            }
-        }
+let priceData = [
+    {
+        prc: '$8.00',
+        yrlprc: '$72.00',
+        views: '10K PAGEVIEWS',
+        backgr: '#ECF0FB',
+        yrl: '/ Year',
+        mnth: '/ month'
+    },
+    {
+        prc: '$12.00',
+        yrlprc: '$108.00',
+        views: '50K PAGEVIEWS',
+        backgr: 'linear-gradient(to left,#ECF0FB 75% ,#10D8C4 25% )',
+        yrl: '/ Year',
+        mnth: '/ month'
+    },
+    {
+        prc: '$16.00',
+        yrlprc: '$144.00',
+        views: '100K PAGEVIEWS',
+        backgr: 'linear-gradient(to right, #10D8C4 50%, #ECF0FB 50%)',
+        yrl: '/ Year',
+        mnth: '/ month'
+    },
+    {
+        prc: '$24.00',
+        yrlprc: '$216.00',
+        views: '500K PAGEVIEWS',
+        backgr: 'linear-gradient(to right, #10D8C4 75%, #ECF0FB 25%)',
+        yrl: '/ Year',
+        mnth: '/ month'
+    },
+    {
+        yrlprc: '$324.00',
+        prc: '$32.00',
+        views: '1M PAGEVIEWS',
+        backgr: 'linear-gradient(to right, #10D8C4 100%, #ECF0FB 0%)',
+        yrl: '/ Year',
+        mnth: '/ month'
+    }
+];
 
 
-    });
+range.addEventListener('input', () => {
+    if (toggle.checked == true) {
+        price.textContent = priceData[range.value].yrlprc;
+        priceSpan.textContent = priceData[range.value].yrl;
 
-}
+    } else {
+        price.textContent = priceData[range.value].prc;
+        priceSpan.textContent = priceData[range.value].mnth
 
-function togglefunc() {
-    toggle.addEventListener('click', () => {
-        if (toggle.checked == true) {
-            if (range.value == 1) {
-                pageviews.textContent = '10K PAGEVIEWS';
-                price.textContent = '$72.00';
-                priceSpan.textContent = '/ Year'
-            }
-            else if (range.value == 2) {
-                pageviews.textContent = '50K PAGEVIEWS';
-                price.textContent = '$108.00';
-                priceSpan.textContent = '/ Year'
-            }
-            else if (range.value == 3) {
-                pageviews.textContent = '100K PAGEVIEWS';
-                price.textContent = '$144.00';
-                priceSpan.textContent = '/ Year'
-            }
-            else if (range.value == 4) {
-                pageviews.textContent = '500K PAGEVIEWS';
-                price.textContent = '$216.00';
-                priceSpan.textContent = '/ Year'
-            }
-            else if (range.value == 5) {
-                pageviews.textContent = '1M PAGEVIEWS';
-                price.textContent = '$324.00';
-                priceSpan.textContent = '/ Year'
-            }
-        }
-        else if (toggle.checked == false) {
-            if (range.value == 1) {
-                pageviews.textContent = '10K PAGEVIEWS';
-                price.textContent = `$${8.00.toFixed(2)}`;
+    }
+    pageviews.textContent = priceData[range.value].views;
+    range.style.background = priceData[range.value].backgr
+});
+toggle.addEventListener('click', () => {
+    if (toggle.checked == true) {
+        price.textContent = priceData[range.value].yrlprc;
+        priceSpan.textContent = priceData[range.value].yrl;
 
-            }
-            else if (range.value == 2) {
-                pageviews.textContent = '50K PAGEVIEWS';
-                price.textContent = `$${12.00.toFixed(2)}`;
-            }
-            else if (range.value == 3) {
-                pageviews.textContent = '100K PAGEVIEWS';
-                price.textContent = `$${16.00.toFixed(2)}`;
-            }
-            else if (range.value == 4) {
-                pageviews.textContent = '500K PAGEVIEWS';
-                price.textContent = `$${24.00.toFixed(2)}`;
-            }
-            else if (range.value == 5) {
-                pageviews.textContent = '1M PAGEVIEWS';
-                price.textContent = `$${32.00.toFixed(2)}`;
-            }
-        }
-    })
+    }
+    else {
+        price.textContent = priceData[range.value].prc;
+        priceSpan.textContent = priceData[range.value].mnth
 
-
-}
-
-
-
-
-
-
-
-
+    }
+})
 
